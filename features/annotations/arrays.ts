4d2db58df6dcae987dd9c6eb991e8567cf202ecb
Dev Su Su <suphonemyint.la@gmail.com>
2023-08-15 02:30:50 +0630

    add section 1 2 3

---
+++ features/annotations/arrays.ts
@@ -0,0 +1,8 @@
+const carMakers = ['ford','toyota','chevy'];
+const dates = [new Date(), new Date()];
+
+const carsByMake = [ 
+    ['f150'],['corolla'],['camaro']
+];
+
+const carsByMake1: string[][] = [];
